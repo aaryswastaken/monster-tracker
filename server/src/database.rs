19cb07368a6057c1ba4e00cf028d36fbd4d72149
Aarys SAHLER <aaryswastaken@gmail.com>
2023-05-13 18:24:16 +0200

    minor modiciations

--- server/src/database.rs
+++ server/src/database.rs
@@ -2,8 +2,8 @@ use std::error::Error;
 use std::result::Result;
 
 use mysql::*;
+use mysql::prelude::Queryable;
 
-use crate::provider;
 
 pub struct Update {
     item_id: u16,
@@ -13,16 +13,35 @@ pub struct Update {
     query_epoch: u64
 }
 
+pub struct QueryPart {
+    shop_id: u16,
+    internal_shop_id: u16,
+    shop_name: String,
+    item_id: u16,
+    item_name: String,
+    external_item_id: String,
+    ssc: String
+}
+
 impl Update {
-    fn launch(&self, conn: Conn) -> Result<(), Box<dyn Error>> {
+    fn launch(&self, conn: &mut Conn) -> Result<(), Box<dyn Error>> {
+        panic!("Deprecated");
+
         return Ok(());
     }
 }
 
-pub fn launch_all(conn: Conn, updates: Vec<Update>) -> Result<u16, Box<dyn Error>> {
+pub fn launch_all(conn: &mut Conn, updates: Vec<Update>) -> Result<u16, Box<dyn Error>> {
     return Ok(0);
 }
 
-pub fn get_queries(conn: Conn) -> Vec<provider::Item> {
-    return vec![]
+pub fn get_queries(conn: &mut Conn) -> Result<Vec<QueryPart>, Box<dyn Error>> {
+    let query = format!("select s.sid as 'shop_id', ve.internal_id as 'internal_shop_id', s.name as 'shop_name', i.id as 'item_id', i.name as 'item_name', si.eid as 'external_item_id', s.specific_cookie as 'shop_specific_cookie' from shops s, items i, specific_id si, vendors ve where si.uid = i.id and si.vid = ve.vid and s.vendor = ve.vid;");
+
+    // ssc is for shop specific cookie
+    let output = conn.query_map(query, |(shop_id, internal_shop_id, shop_name, item_id, item_name, external_item_id, ssc)| {
+            return QueryPart { shop_id, internal_shop_id, shop_name, item_id, item_name, external_item_id, ssc}
+        })?;
+
+    return Ok(output);
 }
