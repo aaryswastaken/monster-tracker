f8797ab36f683445ced5c6ca9c17ccd78ccd6d45
Aarys SAHLER <aaryswastaken@gmail.com>
2023-05-15 11:29:12 +0200

    start of log

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,11 @@
 use std::env;
 
+use fern;
+use fern::colors::Color;
+
+use humantime;
+use log::{error, info, trace};
+
 use tokio;
 
 use std::time::Duration;
@@ -19,8 +25,13 @@ fn value_or_default<T, E>(result: Result<T, E>, default: T) -> T {
     }
 }
 
-fn request_wrapper(conn: &mut PooledConn) {
+fn request_wrapper(conn: &mut PooledConn, i: i64) {
+    let log_target = "SC#".to_owned()+&i.to_string();
+
+    trace!(target: &log_target, "Entering new thread");
     let queries = database::get_queries(conn).expect("Hmmmm no queries?????");
+
+    info!(target: &log_target, "There is {} queries to issue", queries.len());
 
     let mut updates: Vec<database::Update> = Vec::new();
 
@@ -28,19 +39,54 @@ fn request_wrapper(conn: &mut PooledConn) {
         query.fetch_and_push_update(&mut updates).expect("Error while pew pew the request");
     }
 
-    println!("Updates is {} long", updates.len());
+    info!(target: &log_target, "Updates is {} long", updates.len());
 
     let tot = database::launch_all(conn, updates).expect("hmmm no updates????");
 
-    print!("Launched {} requests", tot);
+    info!("Launched {} requests", tot);
+}
+
+fn setup_logging() -> Result<(), fern::InitError> {
+    let base_config = fern::Dispatch::new();
+    let color_config = fern::colors::ColoredLevelConfig::new()
+        .trace(Color::Blue)
+        .debug(Color::Blue)
+        .info(Color::Green)
+        .warn(Color::Yellow)
+        .error(Color::Red);
+
+    // Perform allocation-free log formatting
+    base_config.format(move |out, message, record| {
+            out.finish(format_args!(
+                "[{}] ({}) {} - {}",
+                humantime::format_rfc3339(std::time::SystemTime::now()),
+                record.target(),
+                color_config.color(record.level()),
+                message
+            ))
+        })
+        // Add blanket level filter -
+        .level(log::LevelFilter::Trace)
+        // Output to stdout, files, and other Dispatch configurations
+        .chain(std::io::stdout())
+        .chain(fern::log_file("scraper.log")?)
+        // Apply globally
+        .apply()?;
+
+    return Ok(());
 }
 
 const DEFAULT_PERIOD: Duration = Duration::from_secs(15 * 60);
 
 #[tokio::main]
 async fn main() {
-    let username = env::var("DB_USERNAME").expect("No username has been provided");
-    let password = env::var("DB_PASSWORD").expect("No password has been provided");
+    setup_logging().expect("Tkt");
+
+    info!("Starting scraper...");
+
+    let username = env::var("DB_USERNAME").map_err(|_| error!("No username has been provided")).unwrap();
+    let password = env::var("DB_PASSWORD").map_err(|_| error!("No username has been provided")).unwrap();
+
 
     let host = value_or_default(env::var("DB_SCHEMA"), "127.0.0.1".to_string());
 
@@ -55,13 +101,26 @@ async fn main() {
         Err(_) => DEFAULT_PERIOD
     };
 
-    let pool: Pool = database::connect(host, username, password, port, schema).unwrap();
+
+    info!("Trying to connect to the database using {}@{}", username, host);
+    let pool: Pool = database::connect(host, username, password, port, schema)
+            .map_err(|e| error!("Database connection failed with: {}", e)).unwrap();
+
+
+    info!("Starting the loop with a period of {}", period.as_secs() / 60);
+
+    let mut i = 0;
 
     loop {
+        trace!("Getting a new conn from the pool");
         let mut conn: PooledConn = pool.get_conn().expect("There has been an error in the attempt to fetch a new connection");
+
+        trace!("Spawning new thread, SC#{}", i);
         tokio::spawn(async move {
-            request_wrapper(&mut conn);
+            request_wrapper(&mut conn, i);
         });
+
+        i += 1;
 
         thread::sleep(period);
     }
--- server/src/provider.rs
+++ server/src/provider.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
 use std::str;
 
+use log::{error, info, trace};
+
 use std::process::{Command, Stdio};
 use regex::Regex;
 
@@ -27,8 +29,8 @@ fn wrap_ssc(command: &mut Command, raw: String) -> &mut Command {
     let mut temp: &str;
 
     for part in raw.split(",") {
-        let raw = base64::decode(part).unwrap();
-        temp = str::from_utf8(&raw).unwrap();
+        let raw = base64::decode(part).map_err(|e| error!("There has been an issue in the base64 cookie parsing: {}", e)).unwrap();
+        temp = str::from_utf8(&raw).map_err(|e| error!("There has been an issue in the base64 result accumulation: {}", e)).unwrap();
 
         command.arg("--cookie");
         command.arg(temp);
@@ -50,12 +52,15 @@ impl Item for database::QueryPart {
         return match self.internal_shop_id {
             1 => self.get_carrefour(),
 
-            _ => panic!("Not implemented yet :(")
+            _ => {
+                error!("Tried to target an unsupported vendor. Id: {}", self.internal_shop_id);
+                panic!("Not implemented yet :(");
+            }
         }
     }
 
     fn fetch_and_push_update(&self, updates: &mut Vec<database::Update>) -> BRes<()> {
-        let price = self.get_price()?;
+        let price = self.get_price().map_err(|e| error!("There has been an issue in the price fetching method: {}", e)).unwrap();
 
         let update = database::Update::construct(self, price);
 
@@ -72,15 +77,25 @@ impl Item for database::QueryPart {
             .arg(generate_user_agent())
             .stdout(Stdio::piped())
             .spawn()
+            .map_err(|e| error!("There is an issue in the cURL: {}", e))
             .unwrap();
 
-        let midway_raw = curl.wait_with_output().unwrap();
-        let raw = str::from_utf8(&midway_raw.stdout).unwrap();
-        println!("midway trash {}", raw);
+        let midway_raw = curl.wait_with_output()
+                .map_err(|e| error!("There has been an error in the curl stdout fetch: {}", e)).unwrap();
+        let raw = str::from_utf8(&midway_raw.stdout)
+                .map_err(|e| error!("There has been an error in the curl out aggregation: {}", e)).unwrap();
 
-        let re = Regex::new(r#"<span.+class=\"[a-zA-Z0-9\-\_ ]*product-card-price__price--final\"[a-zA-Z0-9\-\_ =\"]*>[\n ]*([0-9\,]*)€[\n ]*<\/span>"#).unwrap();
+        let re = Regex::new(r#"<span.+class=\"[a-zA-Z0-9\-\_ ]*product-card-price__price--final\"[a-zA-Z0-9\-\_ =\"]*>[\n ]*([0-9\,]*)€[\n ]*<\/span>"#)
+                .map_err(|e| error!("There has been an error in the regex: {}", e)).unwrap();
 
-        let re_res = re.captures(raw).unwrap();
+        let re_res = match re.captures(raw) {
+            Some(e) => e,
+            None => {
+                error!("There has been an issue in the capturing group");
+                panic!("see log");
+            }
+        };
+
         let output = match re_res.get(1) {
             Some(e) => e.as_str(),
             None => ""
@@ -88,7 +103,8 @@ impl Item for database::QueryPart {
 
         println!("Output: {}", output);
 
-        let res = output.parse::<f64>()?;
+        let res = output.parse::<f64>()
+            .map_err(|e| error!("There has been an error in the result parsing: {}", e)).unwrap();
 
         return Ok(res);
     }
