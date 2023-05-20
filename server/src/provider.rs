899b50a22289f13fc2936fbc6266a175370c0ad5
Aarys SAHLER <aaryswastaken@gmail.com>
2023-05-20 17:24:07 +0200

    First try lezgo

--- server/src/provider.rs
+++ server/src/provider.rs
@@ -48,6 +48,7 @@ pub trait Item {
 
     fn get_carrefour(&self) -> BRes<f64>;
     fn get_intermarche(&self) -> BRes<f64>;
+    fn get_casino(&self) -> BRes<f64>;
 }
 
 impl Item for database::QueryPart {
@@ -55,6 +56,7 @@ impl Item for database::QueryPart {
         return match self.internal_shop_id {
             1 => self.get_carrefour(),
             2 => self.get_intermarche(),
+            3 => self.get_casino(),
 
             _ => {
                 error!("Tried to target an unsupported vendor. Id: {}", self.internal_shop_id);
@@ -166,5 +168,92 @@ impl Item for database::QueryPart {
             .map_err(|e| error!("There has been an error in the result parsing: {}", e)).unwrap();
 
         return Ok(res);
+    }
+
+    fn get_casino(&self) -> BRes<f64> {
+        trace!("Casino => trying to dig back the shop id");
+
+        let complete_shop_id: Option<String> = {
+            let mut out: Option<String> = None;
+
+            for cookie in self.ssc.to_string().split(",") {
+                let raw = base64::decode(&cookie).map_err(|e| error!("There has been an issue in the base64 cookie parsing: {}", e)).unwrap();
+                let temp = str::from_utf8(&raw).map_err(|e| error!("There has been an issue in the base64 result accumulation: {}", e)).unwrap();
+
+                if temp.starts_with("shopId") {
+                    let raw_sid_split = temp.split("=").collect::<Vec<&str>>();
+
+                    if raw_sid_split.len() == 2 {
+                        out = Some(String::from(raw_sid_split[1]));
+                    }
+                }
+            }
+
+            out
+        };
+
+
+        let e_sid: String = match complete_shop_id {
+            Some(intermediate_id) => {
+                let temporary_split = intermediate_id.split("&").collect::<Vec<&str>>();
+
+                if temporary_split.len() == 2 {
+                    String::from(temporary_split[0])
+                } else {
+                    error!("The id of the shop doesn't seem to be contained inside the provided cookie. Here's more traceback ssc={} intermediate_id={}", &self.ssc, &intermediate_id);
+                    panic!("See tracelog above");
+                }
+            },
+            None => {
+                error!("No complete_shop_id has been extracted. Here is the ssc: {}", &self.ssc);
+                panic!("See tracelog above");
+            }
+        }; // external shop id
+
+
+        let e_pid = self.external_item_id.to_string(); // external product id
+
+
+        let prepared_url = format!("https://www.casino.fr/casinoexpress_web/affichageDetailProduit/{}{}", e_sid, e_pid);
+
+        trace!("Issuing a request to casino.fr, using url:\n   {}", &prepared_url);
+        let curl = wrap_ssc(Command::new("curl")
+            .arg(prepared_url),
+                self.ssc.to_string())
+            .arg("-A")
+            .arg(generate_user_agent())
+            .stdout(Stdio::piped())
+            .spawn()
+            .map_err(|e| error!("There is an issue in the cURL: {}", e))
+            .unwrap();
+
+        let midway_raw = curl.wait_with_output()
+                .map_err(|e| error!("There has been an error in the curl stdout fetch: {}", e)).unwrap();
+        let raw = str::from_utf8(&midway_raw.stdout)
+                .map_err(|e| error!("There has been an error in the curl out aggregation: {}", e)).unwrap();
+
+        let re = Regex::new(r#"itemprop=\"price\">[ \t\n]+([0-9]+,[0-9*]+).*euro"#)
+                .map_err(|e| error!("There has been an error in the regex: {}", e)).unwrap();
+
+        let re_res = match re.captures(raw) {
+            Some(e) => e,
+            None => {
+                error!("There has been an issue in the capturing group");
+                panic!("see log");
+            }
+        };
+
+        let output = match re_res.get(1) {
+            Some(e) => e.as_str(),
+            None => ""
+        }.replace(",", ".");
+
+        println!("Output: {}", output);
+
+        let res = output.parse::<f64>()
+            .map_err(|e| error!("There has been an error in the result parsing: {}", e)).unwrap();
+
+        return Ok(res);
+
     }
 }
